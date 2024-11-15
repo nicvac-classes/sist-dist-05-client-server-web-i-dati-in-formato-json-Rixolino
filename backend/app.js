9e3a6e1c13a4b032c8408a913957e4983dd95f72
Simone Risola #19 <71252421+Rixolino@users.noreply.github.com>
2024-11-15 13:10:03 +0000

    COMPLETO

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,66 @@
 
 
 // INSERIRE QUI IL CODICE INDICATO NELL'ESERCIZIO
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+const dataPath = path.join('backend', 'data', 'studenti.json');
+
+function leggiJsonStudenti() {
+    try {
+        const data = fs.readFileSync(dataPath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Errore lettura file', error);
+        return { studenti:[] };
+    }
+}
+
+function salvaJsonStudenti(data) {
+    try {
+        fs.writeFileSync(dataPath, JSON.stringify(data, null, 4));
+        return true;
+    } catch (error) {
+        console.error('ERRORE scrittura file:', error)
+        return false;
+    }
+}
+
+app.get('/', (req,res) => {
+    const data = leggiJsonStudenti();
+    res.render('index', {studenti: data.studenti });
+})
+
+app.post('/aggiungi-studente', (req,res) => {
+    const data = leggiJsonStudenti();
+
+    let nuovoID = 1;
+    let iMax = 0;
+    for (let i=0; i<data.studenti.length; i++) {
+        if (data.studenti[i].id > data.studenti[iMax].id) {
+            iMax = i;
+            nuovoID = data.studenti[iMax].id + 1;
+        }
+    }
+
+    const nuovoStudente = {
+        id: nuovoID,
+        nome: req.body.nome,
+        crediti: parseInt(req.body.crediti)
+    };
+
+    data.studenti.push(nuovoStudente);
+    salvaJsonStudenti(data);
+
+    res.redirect('/');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
